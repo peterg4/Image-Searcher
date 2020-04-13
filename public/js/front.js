641c8c0365a5ca045ab4b52f625c1a9b7d360925
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-13 15:19:13 -0400

    second chart data

--- public/js/front.js
+++ public/js/front.js
@@ -283,17 +283,29 @@ app.controller("controller", ['$scope','$http',function($scope, $http) {
       var obj =  data.data.data;
       var likes = [];
       var titles = [];
-      
+      var uploads = [];
+      //0e7e3ac9d01db8 token
+      var locations = [{
+
+      }];
+      var colorSet = new am4core.ColorSet();
       for(var i = 0; i < obj.length; i++) {
         var sublikes = 0;
+        var subload = 0;
         for( var j = 0; j < obj[i].data.length; j++) {
+          var userobj = { "title": obj[i].data[j].user.name,
+                          "latitude": 10,
+                          "longitude": 10,
+                          "color": colorSet.next() 
+                         };
           sublikes += obj[i].data[j].likes;
+          subload += obj[i].data[j].user.total_photos;
+          locations[i*j] = userobj;
         }
         likes.push(sublikes);
         titles.push(obj[i].data[0].tags[0].title);
+        uploads.push(subload);
       }
-      console.log(likes);
-      console.log(titles);
       var ctx = document.getElementById('myChart').getContext('2d');
       var myChart = new Chart(ctx, {
         type: 'bar',
@@ -329,10 +341,10 @@ app.controller("controller", ['$scope','$http',function($scope, $http) {
         data: {
           labels: titles,
           datasets: [{
-              label: 'Most Liked Pix by Tag (separated by search instances)',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: likes,
+              label: 'Amount of Photos Uploaded per User by Search/Tag',
+              backgroundColor: 'rgb(132, 99, 255)',
+              borderColor: 'rgb(132, 99, 255)',
+              data: uploads,
             }]
         },
         options: {
@@ -412,102 +424,7 @@ app.controller("controller", ['$scope','$http',function($scope, $http) {
           })
       }
 
-      var colorSet = new am4core.ColorSet();
-
-      imageSeries.data = [ {
-        "title": "Brussels",
-        "latitude": 50.8371,
-        "longitude": 4.3676,
-        "color":colorSet.next()
-      }, {
-        "title": "Copenhagen",
-        "latitude": 55.6763,
-        "longitude": 12.5681,
-        "color":colorSet.next()
-      }, {
-        "title": "Paris",
-        "latitude": 48.8567,
-        "longitude": 2.3510,
-        "color":colorSet.next()
-      }, {
-        "title": "Reykjavik",
-        "latitude": 64.1353,
-        "longitude": -21.8952,
-        "color":colorSet.next()
-      }, {
-        "title": "Moscow",
-        "latitude": 55.7558,
-        "longitude": 37.6176,
-        "color":colorSet.next()
-      }, {
-        "title": "Madrid",
-        "latitude": 40.4167,
-        "longitude": -3.7033,
-        "color":colorSet.next()
-      }, {
-        "title": "London",
-        "latitude": 51.5002,
-        "longitude": -0.1262,
-        "url": "http://www.google.co.uk",
-        "color":colorSet.next()
-      }, {
-        "title": "Peking",
-        "latitude": 39.9056,
-        "longitude": 116.3958,
-        "color":colorSet.next()
-      }, {
-        "title": "New Delhi",
-        "latitude": 28.6353,
-        "longitude": 77.2250,
-        "color":colorSet.next()
-      }, {
-        "title": "Tokyo",
-        "latitude": 35.6785,
-        "longitude": 139.6823,
-        "url": "http://www.google.co.jp",
-        "color":colorSet.next()
-      }, {
-        "title": "Ankara",
-        "latitude": 39.9439,
-        "longitude": 32.8560,
-        "color":colorSet.next()
-      }, {
-        "title": "Buenos Aires",
-        "latitude": -34.6118,
-        "longitude": -58.4173,
-        "color":colorSet.next()
-      }, {
-        "title": "Brasilia",
-        "latitude": -15.7801,
-        "longitude": -47.9292,
-        "color":colorSet.next()
-      }, {
-        "title": "Ottawa",
-        "latitude": 45.4235,
-        "longitude": -75.6979,
-        "color":colorSet.next()
-      }, {
-        "title": "Washington",
-        "latitude": 38.8921,
-        "longitude": -77.0241,
-        "color":colorSet.next()
-      }, {
-        "title": "Kinshasa",
-        "latitude": -4.3369,
-        "longitude": 15.3271,
-        "color":colorSet.next()
-      }, {
-        "title": "Cairo",
-        "latitude": 30.0571,
-        "longitude": 31.2272,
-        "color":colorSet.next()
-      }, {
-        "title": "Pretoria",
-        "latitude": -25.7463,
-        "longitude": 28.1876,
-        "color":colorSet.next()
-      } ];
-
+      imageSeries.data = locations;
 
 
     });
